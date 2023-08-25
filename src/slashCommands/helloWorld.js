cb3c30b6314a1c53029ec88ad3c5f1097bf366ae
EnderMaster08 <javier.jgonzalez@outlook.com>
2023-08-25 17:48:05 -0400

    Replaced hello word command with github command.

--- src/slashCommands/helloWorld.js
+++ src/slashCommands/helloWorld.js
@@ -3,21 +3,21 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName('hello')
+    .setName('github')
     .setDescription(
-      'Will respond with "world"'
+      'Sends the link to the github.'
     ),
 
   async execute(interaction) {
     try {
       const { user } = interaction;
       // Send a direct message to the user
-      await user.send({ content: `World!` });
+      //await user.send({ content: `World!` });
 
       // Respond in the channel the user sent the message in
-      await interaction.reply(`World!`);
+      await interaction.reply(`https://github.com/EnderMaster77/JoseTheCow`);
     } catch (e) {
-      console.log(`There was an error with the 'hello' command: ${e}`);
+      console.log(`There was an error with the 'Github' command: ${e}`);
     }
   },
 };
